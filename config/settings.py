31b941262e83c1ebb7e31acc9d9e5f6beea46634
Ozodbek <ozodbekprimov69@gmail.com>
2025-04-28 14:18:44 +0500

    add env

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s33#vz!=_qo1^5t5_#5z*#xg)7hn1rj2g#gy+3zg!id6wqzl9i'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['iftor.uz', 'www.iftor.uz']
+ALLOWED_HOSTS = ['iftor.uz', 'www.iftor.uz', '127.0.0.1']
 
 
 
